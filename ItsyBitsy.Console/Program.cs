ccc9db8e79512f41bf4b5815d981a4cbb9a8f2a1
nicholasb.za@gmail.com <nicholasb.za@gmail.com>
2020-03-20 22:32:01 +0200

    create solution

---
+++ ItsyBitsy.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItsyBitsy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
