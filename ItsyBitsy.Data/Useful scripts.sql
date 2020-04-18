00299837ed8cb235c1e08dc24c55c9a601fe661f
nicholasb.za@gmail.com <nicholasb.za@gmail.com>
2020-04-18 21:52:21 +0200

    Added useful scripts file

---
+++ ItsyBitsy.Data/Useful scripts.sql
@@ -0,0 +1,7 @@
+﻿select pageO.*, crossRes.DownloadMs + pageO.DownloadTime [All Content DownloadTime]
+from [Page] pageO 
+	cross apply 
+		(select SUM(pageI.DownloadTime) DownloadMs, ParentPageId
+		from [Page] pageI
+		where ParentPageId = PageO.Id
+		group by ParentPageId) crossRes
