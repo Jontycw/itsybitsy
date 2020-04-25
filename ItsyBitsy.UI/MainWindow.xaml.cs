af847d94fcbea0cc7187b12e02d4a8d93e64e9d6
nicholasb.za@gmail.com <nicholasb.za@gmail.com>
2020-04-25 16:14:05 +0200

    Got data binding working, needed to set data context

--- ItsyBitsy.UI/MainWindow.xaml.cs
+++ ItsyBitsy.UI/MainWindow.xaml.cs
@@ -19,44 +19,13 @@ namespace ItsyBitsy.UI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private bool b;
-        public bool FollowExtenalLinks
-        {
-            get { return b; }
-            set
-            {
-                b = value;
-                PropertyChanged(this, new PropertyChangedEventArgs("FollowExtenalLinks"));
-            }
-        }
-
-        public bool DownloadExternalContent;
-        public bool RespectRobots { get; set; }
-        public bool FollowRedirects { get; set; }
-        public bool UseCookies { get; set; }
-        public bool IncludeImages
-        {
-            get { return Settings.Instance.IncludeImages; }
-            set { Settings.Instance.IncludeImages = value; }
-        }
-        public bool IncludeCss
-        {
-            get { return Settings.Instance.IncludeCss; }
-            set { Settings.Instance.IncludeCss = value; }
-        }
-        public bool IncludeJs { get; set; }
-        public bool IncludeJson { get; set; }
-        public bool IncludeOther { get; set; }
-
         public MainWindow()
         {
-            //_ = Settings.Instance; //loads settings
-
+            _ = Settings.Instance;
             InitializeComponent();
+            //DataContext = Settings.Instance;
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
